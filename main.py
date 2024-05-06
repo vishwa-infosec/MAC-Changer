434524c4073c0520e943dbead3db55391117e019
Vishwa <92665375+vishwa-infosec@users.noreply.github.com>
2024-05-06 20:46:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from os import name
 from sys import argv
 
 def printInterfacesLinux():
-    print("[+] Available network interfaces:")
-    subprocess.call(["ifconfig", "-a"])
+    print("[+] Updated Interface:")
+    #print the updated interface details
+
+    subprocess.call(["ifconfig", "{argv[1]}"])
 
 def changeMacAddressLinux(interface, newMac):
     print(f"[+] Changing MAC address for {interface} to {newMac}")
@@ -12,22 +14,22 @@ def changeMacAddressLinux(interface, newMac):
     subprocess.call(["ifconfig", interface, "hw", "ether", newMac])
     subprocess.call(["ifconfig", interface, "up"])
 
-def printInterfacesWindows():
-    print("[+] Available network interfaces:")
-    subprocess.call(["ipconfig", "/all"])
+#def printInterfacesWindows():
+    #print("[+] Available network interfaces:")
+    #subprocess.call(["ipconfig", "/all"])
 
-def changeMacAddressWindows(interface, newMac):
-    print(f"[+] Changing MAC address for {interface} to {newMac}")
-    subprocess.call(["reg", "add", f"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{interface}\Ndi\params\NetworkAddress", "/v", "NetworkAddress", "/t", "REG_SZ", "/d", newMac, "/f"])
-    subprocess.call(["ipconfig", "/release"])
-    subprocess.call(["ipconfig", "/renew"])
+#def changeMacAddressWindows(interface, newMac):
+    #print(f"[+] Changing MAC address for {interface} to {newMac}")
+    #subprocess.call(["reg", "add", f"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{interface}\Ndi\params\NetworkAddress", "/v", "NetworkAddress", "/t", "REG_SZ", "/d", newMac, "/f"])
+    #subprocess.call(["ipconfig", "/release"])
+    #subprocess.call(["ipconfig", "/renew"])
 
 
 if "__main__" == __name__:
     #check the os and execute the command accordingly
-    if name == 'nt':
-        printInterfacesWindows()
-        changeMacAddressWindows(argv[1], argv[2])
-    else:
-        printInterfacesLinux()
-        changeMacAddressLinux(argv[1], argv[2])
+    #if name == 'nt':
+        #printInterfacesWindows()
+        #changeMacAddressWindows(argv[1], argv[2])
+    #else:
+    printInterfacesLinux()
+    changeMacAddressLinux(argv[1], argv[2])
