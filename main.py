da8fa462d2c86564d39eaf047e1d75ac8dac7d02
Vishwa <92665375+vishwa-infosec@users.noreply.github.com>
2024-05-06 21:03:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import subprocess
-#from os import name
 from sys import argv
 
 def printInterfacesLinux():
-    print("[+] Updated Interface:")
     #print the updated interface details
-
+    print("[+] Updated Interface:")
     subprocess.call(["ifconfig", argv[1]])
 
 def changeMacAddressLinux(interface, newMac):
@@ -14,22 +12,17 @@ def changeMacAddressLinux(interface, newMac):
     subprocess.call(["ifconfig", interface, "hw", "ether", newMac])
     subprocess.call(["ifconfig", interface, "up"])
 
-#def printInterfacesWindows():
-    #print("[+] Available network interfaces:")
-    #subprocess.call(["ipconfig", "/all"])
+def printInterfacesWindows():
+    print("[+] Available network interfaces:")
+    subprocess.call(["ipconfig", "/all"])
 
-#def changeMacAddressWindows(interface, newMac):
-    #print(f"[+] Changing MAC address for {interface} to {newMac}")
-    #subprocess.call(["reg", "add", f"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{interface}\Ndi\params\NetworkAddress", "/v", "NetworkAddress", "/t", "REG_SZ", "/d", newMac, "/f"])
-    #subprocess.call(["ipconfig", "/release"])
-    #subprocess.call(["ipconfig", "/renew"])
+def changeMacAddressWindows(interface, newMac):
+    print(f"[+] Changing MAC address for {interface} to {newMac}")
+    subprocess.call(["reg", "add", f"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{interface}\Ndi\params\NetworkAddress", "/v", "NetworkAddress", "/t", "REG_SZ", "/d", newMac, "/f"])
+    subprocess.call(["ipconfig", "/release"])
+    subprocess.call(["ipconfig", "/renew"])
 
 
 if "__main__" == __name__:
-    #check the os and execute the command accordingly
-    #if name == 'nt':
-        #printInterfacesWindows()
-        #changeMacAddressWindows(argv[1], argv[2])
-    #else:
     changeMacAddressLinux(argv[1], argv[2])
     printInterfacesLinux()
